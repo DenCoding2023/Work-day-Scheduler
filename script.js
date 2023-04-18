bdd7f50b35c7104bedf127eb120ca0bfe71f304a
Dennis Luciano <127637377+DenCoding2023@users.noreply.github.com>
2023-04-18 16:04:10 -0400

    added more hours to the schedule, also added time variable

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ $(function () {
   var today = dayjs();
 $('#currentDay').text(today.format('dddd, MMMM D, YYYY, h:mm'));
 
+// Creating the hour variable//
+
+var nineAM = $('#hour-9');
+var tenAm = $('#hour-10');
+
+
   // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
